a50131dea35f54feb9410b975c9a86e5eda00848
danielcagostinho <danielcagostinho@gmail.com>
2020-09-20 20:18:10 -0400

    First Version

--- src/App.js
+++ src/App.js
@@ -1,26 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import WeatherCard from "./components/WeatherCard/WeatherCard";
+
+import axios from "axios";
+
+import "./App.css";
+
+class App extends Component {
+  state = {
+    weathers: [],
+    celsius: true
+  };
+
+  componentDidMount() {
+    axios
+      .get(
+        "http://api.openweathermap.org/data/2.5/forecast?q=Toronto&appid=9ba589e5a109fb22a3833e80ac287319"
+      )
+      .then((response) => {
+        for (let i = 0; i < response.data.list.length; i+= 8){
+          let currentW = response.data.list[i];
+          this.setState({
+            weathers: this.state.weathers.concat([
+              {
+                date: currentW.dt,
+                tempMax: currentW.main.temp_max,
+                weather: currentW.weather[0].main,
+              },
+            ]),
+          })
+        }
+      });
+  }
+
+  toggleUnit = (unit) => {
+    if(unit == "C"){
+      this.setState({celsius: true});
+    } else {
+      this.setState({celsius: false});
+    }
+  }
+
+  render() {
+    let weatherDisplay = this.state.weathers.map((weather) => {
+      return (
+        <WeatherCard
+          date={weather.date}
+          tempMax={weather.tempMax}
+          weather={weather.weather}
+          tempMin={weather.tempMin}
+          celsius={this.state.celsius}
+        />
+      );
+    });
+
+    return (
+      <div className="App">
+        <div className="Header">
+
+        <h1 className="Title">5 Day Forecast <span className="Location"> - Toronto, CA</span></h1>
+        <div className="ToggleContainer">
+
+        <div className={`ToggleButton${this.state.celsius ? " Toggled" : ""}`} onClick={() => this.toggleUnit("C")}>°C</div>
+        <div className={`ToggleButton${!this.state.celsius ? " Toggled" : ""}`} onClick={() => this.toggleUnit("F")}>°F</div>
+        </div>
+        </div>
+        <div className="CardContainer">{weatherDisplay}</div>
+        <div className="Footer">
+          <p className="FooterText"> created by daniel agostinho</p>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/WeatherCard/WeatherCard.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+import './WeatherCard.css';
+
+
+
+import { WiDaySunny, WiDayCloudy, WiDayRain, WiDaySunnyOvercast } from 'weather-icons-react';
+
+const weatherCard = ({ date, tempMax, weather, celsius}) => {
+  var cloud = require('../../assets/Cloud.png');
+  var overcast = require('../../assets/Overcast.png');
+  var rain = require('../../assets/Rain.png');
+
+  // Convert epoch time to date
+  let dateString = new Date(0);
+  dateString.setUTCSeconds(date)
+
+  let tempMaxString = celsius ? Math.round(tempMax - 273.15) : Math.round(tempMax * 9/5 - 459.67);
+
+
+  let icon = null;
+
+  switch (weather) {
+    case 'Clouds':
+      icon = <img src={cloud} />;
+      break;
+    case 'Clear':
+      icon = <img src={overcast} />;
+      break;
+    case 'Rain':
+      icon = <img src={rain} />;
+      break;
+  }
+
+  return ( 
+    <div className="WeatherCard">
+      <p>{dateString.toLocaleString('en-us', {  weekday: 'short' })}</p>
+      {icon}
+      <p>{tempMaxString}°</p>
+    </div>
+   );
+}
+ 
+export default weatherCard;
