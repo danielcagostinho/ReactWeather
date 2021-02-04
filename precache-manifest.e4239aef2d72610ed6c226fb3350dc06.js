84e4c367a67b92ffeee111a75d435a715173b279
danielcagostinho <danielcagostinho@gmail.com>
2021-02-04 11:53:07 -0500

    Updates

---
+++ precache-manifest.e4239aef2d72610ed6c226fb3350dc06.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc6255dcf04b91dc2e0561c4b3b6818f",
+    "url": "/ReactWeather/index.html"
+  },
+  {
+    "revision": "9bd81c939acad153c90a",
+    "url": "/ReactWeather/static/css/main.27aebe71.chunk.css"
+  },
+  {
+    "revision": "6df7b210eb0959b5f823",
+    "url": "/ReactWeather/static/js/2.0ed87513.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactWeather/static/js/2.0ed87513.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9bd81c939acad153c90a",
+    "url": "/ReactWeather/static/js/main.6d7159d9.chunk.js"
+  },
+  {
+    "revision": "bbd5735f33c62c37e099",
+    "url": "/ReactWeather/static/js/runtime-main.8dca13c5.js"
+  }
+]);
